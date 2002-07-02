bd446973b3c8c5d3bd7ab333858ae61e9005d29d
zardus <zardus@636a839e-d62e-0410-834d-a1d0aacdb804>
2002-07-02 23:32:23 +0000

    gutted int32.cpp|h and moved them to input.cpp|h
    git-svn-id: svn+ssh://svn.code.sf.net/p/snowstorm/code/trunk/openglad@114 636a839e-d62e-0410-834d-a1d0aacdb804

---
+++ input.cpp
@@ -0,0 +1,184 @@
+//
+// __interrupt routines library
+// for Gladiator and associated files
+//
+// Created:  02-04-95
+//
+#include "int32.h"
+#include "scankeys.h"
+// Z's script: #include <dos.h>
+// Z's script: #include <conio.h>
+// Z's script: #include <i86.h>
+#include <stdio.h>
+#include <time.h>
+
+unsigned long timer_count=0;
+unsigned long timer_control=0;
+unsigned long start_time=0;
+time_t starttime, endtime;
+tm tmbuffer;
+// Z: might need these two later
+// dostime_t newtime;
+// dosdate_t newdate;
+long keyboard_grabbed=0;
+long timer_grabbed=0;
+
+unsigned short raw_key;
+short key_press_event = 0;    // used to signed key-press
+char key_list[MAXKEYS+JOYKEYS];
+
+long mouse_state[MSTATE];
+long mouse_buttons;
+
+long joy_state[JSTATE];
+
+void (__far __interrupt *old_timer_isr)();
+void (__far __interrupt *old_keyboard_isr)();
+
+
+void change_time(unsigned long new_count)
+{
+}
+
+void __interrupt increment_timer()
+{
+//  if (!(timer_control%DIVISOR))
+//  {
+//    old_timer_isr();
+//    basecall++;
+//  }
+  timer_count++;
+  timer_control++;
+}
+
+void grab_timer()
+{
+  if (timer_grabbed)
+    return;
+  timer_grabbed = 1;
+  old_timer_isr = _dos_getvect(TIME_KEEPER_INT);
+  _dos_setvect(TIME_KEEPER_INT,increment_timer);
+
+}
+
+void release_timer()
+{
+  if (!timer_grabbed)
+    return;
+  timer_grabbed = 0;
+  _dos_setvect(TIME_KEEPER_INT,old_timer_isr);
+}
+
+void reset_timer()
+{
+  timer_count = 0;
+}
+
+long query_timer()
+{
+  return timer_count;
+}
+
+unsigned long query_timer_control()
+{
+  return timer_control;
+}
+
+
+//
+//Keyboard routines
+//
+
+void grab_keyboard()
+{
+}
+
+void release_keyboard()
+{
+}
+
+void __interrupt key_int()
+{
+  // above fetches a keypress from keyboard, and then
+  // reenables both __interrupts and the keyboard control
+  //remember cli/sti
+}
+
+short query_key()
+{
+  return raw_key;
+}
+
+//
+// Set the keyboard array to all zeros, the
+// virgin state, nothing depressed
+//
+void clear_keyboard()
+{
+  unsigned char i;
+
+  for (i=0; i < MAXKEYS+JOYKEYS; i++)
+   key_list[i] = 0;  // 0 is off, of course
+
+}
+
+char * query_keyboard()
+{
+  return key_list;
+}
+
+void wait_for_key(unsigned char somekey)
+{
+}
+
+short query_key_press_event()
+{
+  return key_press_event;
+}
+
+void clear_key_press_event()
+{
+  key_press_event = 0;
+}
+
+short query_key_code(short code)
+{
+  return key_list[code];
+}
+
+
+//
+// Mouse routines
+//
+
+void grab_mouse()
+{
+}
+
+void release_mouse()
+{
+}
+
+void reset_mouse()
+{
+}
+
+long * query_mouse()
+{
+  //Fetches buttons, and sets mouse x,y coords
+  //mouse_state[MOUSE_LEFT] = (long) (mouse_buttons & (long) LEFT_MASK);
+  //mouse_state[MOUSE_RIGHT] = (long) (mouse_buttons & (long) RIGHT_MASK);
+  //mouse_state[MOUSE_X] = (long) (outregs.w.cx >> 1);
+  //mouse_state[MOUSE_Y] = (long) outregs.w.dx;
+  //return mouse_state;
+}
+
+long * query_joy()
+{
+  //joyasm();
+  //joy_state[JOY_X] = (long) joyx;
+  //joy_state[JOY_Y] = (long) joyy;
+  
+  //return joy_state;
+}
+
