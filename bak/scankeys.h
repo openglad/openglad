c9da6c5b66f98a1786f61e5b31122e8f97311b47
zardus <zardus@636a839e-d62e-0410-834d-a1d0aacdb804>
2002-07-05 04:47:26 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.code.sf.net/p/snowstorm/code/trunk/openglad@147 636a839e-d62e-0410-834d-a1d0aacdb804

---
+++ bak/scankeys.h
@@ -0,0 +1,116 @@
+// Scan-key codes ..
+// Picture Object class defs
+
+#ifndef SCANKEYS_H
+#define SCANKEYS_H
+
+// Define Keypad..
+
+#define SCAN_NUMLOCK    69
+#define SCAN_NUMERIC_DIVIDE     53
+#define SCAN_NUMERIC_TIMES      55
+#define SCAN_NUMERIC_MINUS      74
+#define SCAN_NUMERIC_DASH       74
+#define SCAN_NUMERIC_PLUS       78
+#define SCAN_NUMERIC_ENTER      28
+
+#define SCAN_HOME       71
+#define SCAN_UP         72
+#define SCAN_PGUP       73
+#define SCAN_LEFT       75
+#define SCAN_CENTER     76
+#define SCAN_RIGHT      77
+#define SCAN_PLUS       78
+#define SCAN_END        79
+#define SCAN_DOWN       80
+#define SCAN_PGDN       81
+
+#define SCAN_INSERT     82
+#define SCAN_DELETE     83
+#define SCAN_ENTER      28
+
+// Numeric Row
+#define SCAN_OPEN_QUOTE  41
+#define SCAN_1  2
+#define SCAN_2  3
+#define SCAN_3  4
+#define SCAN_4  5
+#define SCAN_5  6
+#define SCAN_6  7
+#define SCAN_7  8
+#define SCAN_8  9
+#define SCAN_9  10
+#define SCAN_0  11
+#define SCAN_MINUS  12
+#define SCAN_DASH   12
+#define SCAN_EQUALS  13
+#define SCAN_BACKSPACE  14
+
+// QWERTY row
+#define SCAN_TAB        15
+#define SCAN_Q  16
+#define SCAN_W  17
+#define SCAN_E  18
+#define SCAN_R  19
+#define SCAN_T  20
+#define SCAN_Y  21
+#define SCAN_U  22
+#define SCAN_I  23
+#define SCAN_O  24
+#define SCAN_P  25
+#define SCAN_LEFT_BRACKET  26
+#define SCAN_OPEN_BRACKET  26     
+#define SCAN_RIGHT_BRACKET  27
+#define SCAN_CLOSE_BRACKET  27
+
+// ASDF Row
+#define SCAN_A  30  
+#define SCAN_S  31
+#define SCAN_D  32      
+#define SCAN_F  33
+#define SCAN_G  34
+#define SCAN_H  35
+#define SCAN_J  36
+#define SCAN_K  37
+#define SCAN_L  38
+#define SCAN_SEMICOLON  39
+#define SCAN_CLOSE_QUOTE  40
+#define SCAN_ENTER 28
+
+// ZXCV Row
+#define SCAN_LEFT_SHIFT 42
+#define SCAN_Z  44
+#define SCAN_X  45
+#define SCAN_C  46
+#define SCAN_V  47
+#define SCAN_B  48
+#define SCAN_N  49
+#define SCAN_M  50
+#define SCAN_COMMA  51
+#define SCAN_PERIOD  52     
+#define SCAN_SLASH  53
+#define SCAN_DIVIDE 53
+#define SCAN_RIGHT_SHIFT 54
+
+// Spacebar row
+#define SCAN_CTRL  29
+#define SCAN_ALT   56
+#define SCAN_SPACE 57
+
+// Function keys
+#define SCAN_ESC 1
+#define SCAN_ESCAPE 1
+#define SCAN_F1  59
+#define SCAN_F2  60
+#define SCAN_F3  61
+#define SCAN_F4  62
+#define SCAN_F5  63
+#define SCAN_F6  64
+#define SCAN_F7  65
+#define SCAN_F8  66
+#define SCAN_F9  67
+#define SCAN_F10 68
+#define SCAN_F11 87
+#define SCAN_F12 88
+                     
+#endif
