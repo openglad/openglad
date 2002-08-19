e80ea79012d9dd75d3b6552a9409ef20d5a80405
zardus <zardus@636a839e-d62e-0410-834d-a1d0aacdb804>
2002-08-19 05:15:55 +0000

    now makes a semi-valid file
    git-svn-id: svn+ssh://svn.code.sf.net/p/snowstorm/code/trunk/openglad@378 636a839e-d62e-0410-834d-a1d0aacdb804

--- util/pack.c
+++ util/pack.c
@@ -3,42 +3,56 @@
 
 #define FILENAME_SIZE 13
 
-/*class packfileinfo
-{
-	public:
-		long filepos;
-		char name[FILENAME_SIZE];
-};*/
-
 int main(int argc, char **argv)
 {
 	short numfiles = (short) argc - 1;
 	long *filelocation;
+	long *filesize;
 	long sizeoffile;
 	int i;
 	FILE *infile;
 	FILE *outfile;
-//	packfileinfo *fileinfo;
-	char buffer[50000]; //buffers: malloc is for wimps
+	char buffer[5000000]; //buffers: malloc is for wimps
 
 	filelocation = (long *) malloc(sizeof(long) * argc);
-//	fileinfo = new packfileinfo[numfiles];
+	filesize = (long *) malloc(sizeof(long) * argc);
 
 	filelocation[0] = 0;
-	if (!(outfile = fopen("output.001", "w"))) return 1;
+	if (!(outfile = fopen("outfile.001", "w"))) return 1;
+	fwrite("GladPack", 8, 1, outfile);
+	fwrite(&numfiles, sizeof(short), 1, outfile);
 
 	for (i = 0; i < numfiles; i++)
 	{
 		if (!(infile = fopen(argv[i + 1], "rb"))) exit(0);
-		printf("adding %s\n",argv[i+1]);
 		fseek(infile, 0, SEEK_END);
-		filelocation[i] = ftell(infile);
-		printf("-- size %d\n",filelocation[i]);
-		//buffers: fseek(infile, - ftell(infile), SEEK_END);
-		fread(buffer,filelocation[i],1,infile);
-		fwrite(buffer, filelocation[i], 1, outfile);
+		filesize[i] = ftell(infile);
+		filelocation[i + 1] = filelocation[i] + filesize[i];
+
 		fclose(infile);
 	}
+
+	sizeoffile = filelocation[numfiles] + filesize[numfiles - 1];
+	fwrite(&sizeoffile, sizeof(long), 1, outfile);
+
+	for (i = 0; i < numfiles; i++)
+	{
+		fwrite(argv[i+1], sizeof(char) * 13, 1, outfile);
+		fwrite(&filelocation[i], sizeof(long), 1, outfile);
+	}
+
+	for (i = 0; i < numfiles; i++)
+	{
+		if (!(infile = fopen(argv[i + 1], "rb"))) exit(0);
+		printf("adding %s ",argv[i+1]);
+		printf("-- size %d\n",filesize[i]);
+
+		fread(buffer,filesize[i],1,infile);
+		fwrite(buffer, filesize[i], 1, outfile);
+
+		fclose(infile);
+	}
+
 
 	fclose(outfile);
 }
