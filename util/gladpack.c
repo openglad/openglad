4cd2a3a57540dbd02259de12785baff8aa04273b
zardus <zardus@636a839e-d62e-0410-834d-a1d0aacdb804>
2002-08-21 02:26:22 +0000

    evolving gladpack.c
    git-svn-id: svn+ssh://svn.code.sf.net/p/snowstorm/code/trunk/openglad@402 636a839e-d62e-0410-834d-a1d0aacdb804

--- util/gladpack.c
+++ util/gladpack.c
@@ -8,7 +8,18 @@
 
 int main(int argc, char **argv)
 {
-	short numfiles = (short) argc - 2;
+	if (argc > 1 && !strcmp(argv[1], "p")) pack(argc, argv);
+	else if (argc > 1 && !strcmp(argv[1], "u")) unpack(argc, argv);
+	else
+	{
+		printf("Usage: gladpack (p|u) ...\n");
+		return 1;
+	}
+}
+
+int pack(int argc, char **argv)
+{
+	short numfiles = (short) argc - 3;
 	long *filelocation;
 	long *filesize;
 	long sizeoffile;
@@ -17,23 +28,25 @@ int main(int argc, char **argv)
 	FILE *outfile;
 	char buffer[5000000]; //buffers: malloc is for wimps
 
-	if (numfiles <= 1)
+	if (numfiles < 1)
 	{
-		printf("Usage: gladpack outfile.001 file1 [file2] [file3] ...\n");
-		return 0;
+		printf("Usage: gladpack p outfile.001 file1 [file2] [file3] ...\n");
+		return 1;
 	}
+
+	printf ("Creating packfile %s...\n", argv[2]);
 
 	filelocation = (long *) malloc(sizeof(long) * (numfiles + 1));
 	filesize = (long *) malloc(sizeof(long) * (numfiles + 1));
 
 	filelocation[0] = 8 + sizeof(short) + numfiles * (13 + sizeof(long)) + sizeof(long);
-	if (!(outfile = fopen(argv[1], "w"))) return 1;
+	if (!(outfile = fopen(argv[2], "w"))) return 1;
 	fwrite("GladPack", 8, 1, outfile);
 	fwrite(&numfiles, sizeof(short), 1, outfile);
 
 	for (i = 0; i < numfiles; i++)
 	{
-		if (!(infile = fopen(argv[i + 2], "rb"))) exit(0);
+		if (!(infile = fopen(argv[i + 3], "rb"))) exit(0);
 		fseek(infile, 0, SEEK_END);
 		filesize[i] = ftell(infile);
 		filelocation[i + 1] = filelocation[i] + filesize[i];
@@ -46,16 +59,14 @@ int main(int argc, char **argv)
 
 	for (i = 0; i < numfiles; i++)
 	{
-		fwrite(argv[i+2], sizeof(char) * 13, 1, outfile);
+		fwrite(argv[i+3], sizeof(char) * 13, 1, outfile);
 		fwrite(&filelocation[i + 1], sizeof(long), 1, outfile);
 	}
 
-	printf("So far: %i\n", ftell(outfile));
-
 	for (i = 0; i < numfiles; i++)
 	{
-		if (!(infile = fopen(argv[i + 2], "rb"))) exit(0);
-		printf("adding %s ",argv[i+2]);
+		if (!(infile = fopen(argv[i + 3], "rb"))) exit(0);
+		printf("adding %s ",argv[i+3]);
 		printf("-- location %i ", filelocation[i]);
 		printf("-- size %d\n",filesize[i]);
 
@@ -67,4 +78,8 @@ int main(int argc, char **argv)
 
 
 	fclose(outfile);
+}
+
+int unpack(int argc, char **argv)
+{
 }
