f19266031690aed7441cd4fb6657508399d49898
aglswapbuffers <aglswapbuffers@636a839e-d62e-0410-834d-a1d0aacdb804>
2002-08-28 05:55:18 +0000

    forgetting to uncomment and just not commiting are two different things...
    git-svn-id: svn+ssh://svn.code.sf.net/p/snowstorm/code/trunk/openglad@494 636a839e-d62e-0410-834d-a1d0aacdb804

--- src/screen.cpp
+++ src/screen.cpp
@@ -1,5 +1,5 @@
 /* Copyright (C) 1995-2002  FSGames. Ported by Sean Ford and Yan Shosh
- *
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
