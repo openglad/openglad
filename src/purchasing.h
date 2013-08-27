e55cc988f4afdb72e5e6cf27e1ab8110aef7756f
Jonathan Dearborn <grimfang4@gmail.com>
2013-08-27 12:27:35 -0400

    Added purchasing interface for Ouya.

---
+++ src/purchasing.h
@@ -0,0 +1,22 @@
+#ifndef _PURCHASING_H__
+#define _PURCHASING_H__
+
+#include <string>
+#include <vector>
+
+class ProductInfo
+{
+public:
+    std::string id;
+    int priceInCents;
+    std::string name;
+};
+
+void buyProduct(const std::string& id);
+ProductInfo getProductInfo(const std::string& id);
+std::vector<std::string> getAllProducts();
+bool doesOwnProduct(const std::string& id);
+
+void test_purchasing();
+
+#endif
