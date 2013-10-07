bc01b428eb33ce0f43485d6543e8c2cd98cae4d2
Jonathan Dearborn <grimfang4@gmail.com>
2013-10-06 23:41:54 -0400

    Changed the XP from attacks so you still get some XP when you beat weaklings.

--- src/walker.cpp
+++ src/walker.cpp
@@ -1551,10 +1551,10 @@ short walker::collide(walker  *ob)
 short get_xp_from_attack(walker* w, walker* target, float damage)
 {
     float x = (w->stats->level - target->stats->level);
-    // Whooo-ee!  An interpolated (quintic) polynomial to fit {{0,30},{1,15},{2,5},{3,1.5},{4,0.5},{5,0},{7,-50}} for 20 damage done.
+    // Whooo-ee!  An interpolated (quintic) polynomial to fit {{0,30},{1,25},{2,15},{3,10},{4,5},{5,2.5},{6,1.25},{7,0.5},{8,0.25},{9,-10}} for 20 damage done.
     // Being an odd order polynomial is important so it can rise to infinity leftward and fall to neg infinity rightward.
     // The factor was adjusted to make level ups happen at a good rate.
-    float poly = -0.017881*pow(x,5)+0.137265*pow(x,4)-0.434659*pow(x,3)+3.42733*pow(x,2)-18.274*x+30.0237;
+    float poly = -0.00246795*pow(x,5)+0.013243*pow(x,4)+0.223208*pow(x,3)-1.16091*pow(x,2)-5.54277*x+30.2923;
     float result = 6.0f*damage*poly/20.0f;
     if(result <= 0)
         return 0;
